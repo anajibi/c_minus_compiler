6e5b7713a991524e87925ca308529cbe7d498b73
Ali Najibi <njibiali@gmail.com>
2021-10-17 17:47:00 +0330

    complete implementation of number token (errors have not been implemented yet.

--- compiler.py
+++ compiler.py
@@ -1,20 +1,19 @@
 from scanner import get_next_token
 from constants import TOKENS_FILE_NAME
 
-tokens_file = open(TOKENS_FILE_NAME, "a")
-
-tokens_list = []
-while (token := get_next_token()) is not None:
-    if tokens_list[token[1]]:
-        tokens_list[token[1]].append(token[0])
-    else:
-        tokens_list[token[1]] = token[0]
+tokens_file = open(TOKENS_FILE_NAME, "w")
 index = 0
-for token_line, in tokens_list:
-    global index
+tokens_list = []
+
+while (token := get_next_token()) is not None:
+    if len(tokens_list) < token[1]:
+        tokens_list.append([token[0]])
+    else:
+        tokens_list[token[1] - 1].append(token[0])
+for token_line in tokens_list:
     index += 1
     tokens_file.write(f"{index}.\t")
     for token in token_line:
-        tokens_file.write(f"{token} ")
+        tokens_file.write(f"({token.type}, {token.lexeme}) ")
 
     tokens_file.write("\n")
