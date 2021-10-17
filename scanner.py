21fadabdec9158765943cd439fe7af30fedcae94
Ali Najibi <njibiali@gmail.com>
2021-10-18 00:20:19 +0330

    fix bugs

--- scanner.py
+++ scanner.py
@@ -2,7 +2,7 @@ from constants import LEXICAL_ERRORS_FILE_NAME, INPUT_FILE_NAME, KEYWORDS
 import re
 
 symbol_table = {}
-lexical_error_log = open(LEXICAL_ERRORS_FILE_NAME, "a")
+lexical_error_log = open(LEXICAL_ERRORS_FILE_NAME, "w")
 input_file = open(INPUT_FILE_NAME, "r", encoding="UTF-8")
 current_lexeme = ""
 buffer = ""
@@ -52,19 +52,21 @@ def get_next_token():
     elif is_letter(buffer[0]):  # For Keywords & IDs
         append_to_current_lexeme()
         read_keyword_or_id()
-        if current_lexeme in dict(list(symbol_table.items())[:7]):
+        if current_lexeme in dict(list(symbol_table.items())[:8]):
             read_token = Token("KEYWORD", current_lexeme)
         else:
             read_token = Token("ID", current_lexeme)
     elif is_starting_comment(buffer[0]):
         append_to_current_lexeme()
         read_comment()
+        current_lexeme = ""
         return get_next_token()
     elif is_symbol(buffer[0]):
         append_to_current_lexeme()
         read_symbol()
         read_token = Token("SYMBOL", current_lexeme)
     else:
+        append_to_current_lexeme()
         report_error("Invalid input")
         return get_next_token()
     current_lexeme = ""
@@ -98,7 +100,7 @@ def read_number():
     while buffer != "" and is_digit(buffer[0]):
         append_to_current_lexeme()
     if buffer[0] and (not (is_symbol(buffer[0]) | is_white_space(buffer[0]) | is_starting_comment(buffer[0]))):
-        # Todo: Symbol would've not been invalid?!
+        append_to_current_lexeme()
         report_error("Invalid number")
 
 
@@ -106,27 +108,36 @@ def read_keyword_or_id():
     while buffer != "" and (is_digit(buffer[0]) or is_letter(buffer[0])):
         append_to_current_lexeme()
     if buffer[0] and (not (is_symbol(buffer[0]) | is_white_space(buffer[0]) | is_starting_comment(
-            buffer[0]))):  # Todo: Couldn't figure out this?
+            buffer[0]))):
+        append_to_current_lexeme()
         report_error("Invalid input")
 
 
 def read_and_dump_comment(stop_char):
-    while buffer[0] and buffer[0] != stop_char:
+    while buffer != "" and buffer[0] != stop_char:
         append_to_current_lexeme()
 
 
+def shorten(lexeme):
+    return f"{lexeme[:7]}{'...' if len(lexeme) > 7 else ''}"
+
+
 def read_comment():
+    starting_line = line_number
+    global current_lexeme
     if buffer != "" and is_starting_comment(buffer[0]):
         append_to_current_lexeme()
         while buffer != "" and buffer[0] != "\n":
             append_to_current_lexeme()
     elif buffer != "" and re.fullmatch("[*]", buffer[0]) is not None:
+        append_to_current_lexeme()
         while True:
             read_and_dump_comment("*")
             if buffer == "":
                 read_new_line()
                 if buffer == "":
-                    report_error("Unclosed comment")
+                    lexical_error_log.write(f"{starting_line}.\t({shorten(current_lexeme)}, Unclosed comment) ")
+                    current_lexeme = ""
                     return
             else:
                 append_to_current_lexeme()
@@ -134,11 +145,19 @@ def read_comment():
                     append_to_current_lexeme()
                     return
     else:
+        if buffer != "":
+            append_to_current_lexeme()
         report_error("Invalid input")
 
 
 def read_symbol():
-    pass  # Todo
+    if current_lexeme == "=":
+        if buffer != "" and buffer[0] == "=":
+            append_to_current_lexeme()
+    elif current_lexeme == "*":
+        if buffer != "" and buffer[0] == "/":
+            append_to_current_lexeme()
+            report_error("Unmatched comment")
 
 
 def report_error(typ):
@@ -150,7 +169,7 @@ def report_error(typ):
 def write_lexical_errors():
     global lexical_errors
     if len(lexical_errors) != 0:
-        lexical_error_log.write(f"{line_number}.\t")
+        lexical_error_log.write(f"{line_number - 1}.\t")
         error: LexicalError
         for error in lexical_errors:
             lexical_error_log.write(f"({error.dumped_lexeme}, {error.error_type}) ")
