c9c1495afb00cea9d1f43efda16d5b29e2f68f59
Ali Najibi <njibiali@gmail.com>
2021-10-17 16:49:52 +0330

    add mechanism for dumping lexical errors

--- scanner.py
+++ scanner.py
@@ -4,20 +4,45 @@ symbol_table = set()
 lexical_error_log = open(LEXICAL_ERRORS_FILE_NAME, "a")
 input_file = open(INPUT_FILE_NAME, "r", encoding="UTF-8")
 current_lexeme = ""
-current_buffer = ""
+buffer = ""
 line_number = 0
+lexical_errors = []
+
+
+def read_new_line():
+    global buffer
+    global line_number
+    buffer = input_file.readline()
+    line_number += 1
+    write_lexical_errors()
 
 
 def get_next_token():
-    global current_buffer
+    global buffer
     global line_number
-    if current_buffer == "":
-        current_buffer = input_file.readline()
-        line_number += 1
-    if current_buffer == "":
+    if buffer == "":
+        read_new_line()
+
+    if buffer == "":
         return None
     pass;
 
 
-def report_lexical_error(dumped_lexeme, error_type):
-    lexical_error_log.write(f"({dumped_lexeme}, {error_type}) ")
+def write_lexical_errors():
+    global lexical_errors
+    if len(lexical_errors) != 0:
+        lexical_error_log.write(f"{line_number}.\t")
+        error: LexicalError
+        for error in lexical_errors:
+            lexical_error_log.write(f"({error.dumped_lexeme}, {error.error_type}) ")
+        lexical_error_log.write("\n")
+    lexical_errors = []
+
+
+class LexicalError:
+    dumped_lexeme: str
+    error_type: str
+
+    def __init__(self, dumped_lexeme, error_type):
+        self.dumped_lexeme = dumped_lexeme
+        self.error_type = error_type
