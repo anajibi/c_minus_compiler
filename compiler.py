c1bd91c74e0f8fea2a0ccd0dd155c371a27d10b1
Ali Najibi <njibiali@gmail.com>
2021-10-18 23:44:42 +0330

    change structure of print_tokens, print_lexical_errors, print_symbol_table

--- compiler.py
+++ compiler.py
@@ -1,26 +1,30 @@
-from scanner import get_next_token, init_keywords
+from scanner import get_next_token, init_keywords, print_symbol_table, print_lexical_errors
 from constants import TOKENS_FILE_NAME
 
 
 def main():
-    tokens_file = open(TOKENS_FILE_NAME, "w")
-    index = 0
     tokens_list = dict()
+
+    def print_tokens():
+
+        tokens_file = open(TOKENS_FILE_NAME, "w")
+        for index, token_line in tokens_list.items():
+            tokens_file.write(f"{index + 1}.\t")
+            for token in token_line:
+                tokens_file.write(f"({token.type}, {token.lexeme}) ")
+            tokens_file.write("\n")
 
     init_keywords()
 
     while (token := get_next_token()) is not None:
-        if tokens_list.get(token[1] - 1) is None:
-            tokens_list[token[1] - 1] = [token[0]]
+        if tokens_list.get(token.line_num - 1) is None:
+            tokens_list[token.line_num - 1] = [token]
         else:
-            tokens_list[token[1] - 1].append(token[0])
-    for _, token_line in tokens_list.items():
-        index += 1
-        tokens_file.write(f"{index}.\t")
-        for token in token_line:
-            tokens_file.write(f"({token.type}, {token.lexeme}) ")
+            tokens_list[token.line_num - 1].append(token)
 
-        tokens_file.write("\n")
+    print_tokens()
+    print_symbol_table()
+    print_lexical_errors()
 
 
 if __name__ == '__main__':
