bc62989c0abdc092a0df3b9d9cb7dc2fd9628311
Ali Najibi <njibiali@gmail.com>
2021-10-23 16:09:06 +0330

    add testRunner.py for running all tests

---
+++ testRunner.py
@@ -0,0 +1,27 @@
+import os
+
+from constants import LEXICAL_ERRORS_FILE_NAME, SYMBOL_TABLE_FILE_NAME, TOKENS_FILE_NAME
+
+
+def compare():
+    index = 0
+    while index < 11:
+        test_case_id = "C:\\Users\\Ali Najibi\\Desktop\\PA1_testcases1.3\\T10\\"
+        input_txt = open(test_case_id + "input.txt", "r").read()
+        open("input.txt", "w").write(input_txt)
+        os.system('python compiler.py')
+        le = open(test_case_id + "lexical_errors.txt", "r").read()
+        st = open(test_case_id + "symbol_table.txt", "r").read()
+        t = open(test_case_id + "tokens.txt", "r").read()
+        le_m = open(LEXICAL_ERRORS_FILE_NAME, "r").read()
+        st_m = open(SYMBOL_TABLE_FILE_NAME, "r").read()
+        t_m = open(TOKENS_FILE_NAME, "r").read()
+
+        print(le == le_m, "lexical_errors")
+        print(st == st_m, "Symbol Table")
+        print(t == t_m, "Tokens")
+        print("=========================================")
+        index += 1
+
+
+compare()
