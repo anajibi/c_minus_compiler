fc6d36b44dde702d6e49409a1f48b55b0f04586c
Alireza <honarvar45@gmail.com>
2022-01-29 08:18:51 +0330

    pnum

--- declarations.py
+++ declarations.py
@@ -4,7 +4,7 @@ from typing import Union, List
 
 class ActionSymbol(Enum):
     ptoken = "ptoken",
-    pid = "pid",
+    pid = "pid", # Todo: There was some pnum, wasn't anywhere else. What was it?
     starr = "starr",
     stvar = "stvar",
     stfunc = "stfunc",
